c6b6d5358ac896dbeeb6b3af19794d14481a06ec
Scarsnik <leo.stefani@wanadoo.fr>
2023-04-28 18:45:56 +0200

    Fix warning

--- src/components/training/CardTraining.tsx
+++ src/components/training/CardTraining.tsx
@@ -53,6 +53,7 @@ export default function CardTraining(props: Props) {
         return () => {
             document.removeEventListener("keyup", handleShortcut);
         };
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     useEffect(() => {
